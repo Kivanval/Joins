c99810403bd9caedfce50a203758bacf93a843ce
kivanval <kivanval@gmail.com>
2022-08-03 19:54:26 +0300

    Advanced Frame #2

--- src/main/java/org/example/JoinOperationUtils.java
+++ src/main/java/org/example/JoinOperationUtils.java
@@ -7,19 +7,12 @@ public final class JoinOperationUtils {
     private JoinOperationUtils() {
     }
 
-    public static <T1, T2> int minSize(Collection<T1> leftCollection, Collection<T2> rightCollection) {
+    static <T1, T2> int minSize(Collection<T1> leftCollection, Collection<T2> rightCollection) {
         return Math.min(leftCollection.size(), rightCollection.size());
     }
 
-    public static <K, V> Map<K, V> dataRowCollectionToHashMap(Collection<DataRow<K, V>> dataRows) {
+    static <K, V> Map<K, V> dataRowCollectionToHashMap(Collection<DataRow<K, V>> dataRows) {
         return dataRows.stream().collect(Collectors.toMap(DataRow::getKey, DataRow::getValue));
     }
-
-    public static <K extends Comparable<K>, V> Set<DataRow<K, V>> dataRowCollectionToIndex(Collection<DataRow<K, V>> dataRows) {
-        TreeSet<DataRow<K, V>> index = new TreeSet<>(Comparator.comparing(DataRow::getKey));
-        index.addAll(dataRows);
-        return index;
-    }
-
 
 }
