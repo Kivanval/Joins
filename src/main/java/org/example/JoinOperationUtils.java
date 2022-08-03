977a9f00d42f50276c384cd256929503d5585610
kivanval <kivanval@gmail.com>
2022-08-03 19:23:44 +0300

    Advanced Frame #1

--- src/main/java/org/example/JoinOperationUtils.java
+++ src/main/java/org/example/JoinOperationUtils.java
@@ -1,6 +1,7 @@
 package org.example;
 
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public final class JoinOperationUtils {
     private JoinOperationUtils() {
@@ -8,6 +9,16 @@ public final class JoinOperationUtils {
 
     public static <T1, T2> int minSize(Collection<T1> leftCollection, Collection<T2> rightCollection) {
         return Math.min(leftCollection.size(), rightCollection.size());
+    }
+
+    public static <K, V> Map<K, V> dataRowCollectionToHashMap(Collection<DataRow<K, V>> dataRows) {
+        return dataRows.stream().collect(Collectors.toMap(DataRow::getKey, DataRow::getValue));
+    }
+
+    public static <K extends Comparable<K>, V> Set<DataRow<K, V>> dataRowCollectionToIndex(Collection<DataRow<K, V>> dataRows) {
+        TreeSet<DataRow<K, V>> index = new TreeSet<>(Comparator.comparing(DataRow::getKey));
+        index.addAll(dataRows);
+        return index;
     }
 
 
--- src/main/java/org/example/LeftJoinOperation.java
+++ src/main/java/org/example/LeftJoinOperation.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 public class LeftJoinOperation<K, V1, V2>
         implements JoinOperation<DataRow<K, V1>, DataRow<K, V2>, JoinedDataRow<K, V1, V2>> {
@@ -10,25 +11,38 @@ public class LeftJoinOperation<K, V1, V2>
                                                      Collection<DataRow<K, V2>> rightCollection) {
         ArrayList<JoinedDataRow<K, V1, V2>> resultCollection = new ArrayList<>(JoinOperationUtils
                 .minSize(leftCollection, rightCollection));
-        for (DataRow<K, V1> leftDataRow : leftCollection) {
-            boolean hasJoin = false;
-            for (DataRow<K, V2> rightDataRow : rightCollection) {
-                if (leftDataRow.getKey().equals(rightDataRow.getKey())) {
-                    JoinedDataRow<K, V1, V2> joinedDataRow = new JoinedDataRow<>(
-                            leftDataRow,
-                            rightDataRow
-                    );
-                    resultCollection.add(joinedDataRow);
-                    hasJoin = true;
-                }
-            }
-            if (!hasJoin) {
+        if (leftCollection.size() >= rightCollection.size()) {
+            Map<K, V2> hashMap = JoinOperationUtils.dataRowCollectionToHashMap(rightCollection);
+            for (DataRow<K, V1> leftDataRow : leftCollection) {
+                V2 rightValue = hashMap.get(leftDataRow.getKey());
                 JoinedDataRow<K, V1, V2> joinedDataRow = new JoinedDataRow<>(
                         leftDataRow.getKey(),
                         leftDataRow.getValue(),
-                        null
+                        rightValue
                 );
                 resultCollection.add(joinedDataRow);
+            }
+        } else {
+            for (DataRow<K, V1> leftDataRow : leftCollection) {
+                boolean hasJoin = false;
+                for (DataRow<K, V2> rightDataRow : rightCollection) {
+                    if (leftDataRow.getKey().equals(rightDataRow.getKey())) {
+                        JoinedDataRow<K, V1, V2> joinedDataRow = new JoinedDataRow<>(
+                                leftDataRow,
+                                rightDataRow
+                        );
+                        resultCollection.add(joinedDataRow);
+                        hasJoin = true;
+                    }
+                }
+                if (!hasJoin) {
+                    JoinedDataRow<K, V1, V2> joinedDataRow = new JoinedDataRow<>(
+                            leftDataRow.getKey(),
+                            leftDataRow.getValue(),
+                            null
+                    );
+                    resultCollection.add(joinedDataRow);
+                }
             }
         }
         return resultCollection;
